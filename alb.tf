59c71e59d3686445ac9b7064e1dc1887af41c85c
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:28:57 -0500

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
+  security_groups    = 
   subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   enable_deletion_protection = true
