445649916a24901ec16ba6b22d217fef24f11f5e
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:21:06 -0500

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 # Creates Security Group
-resource "aws_security_group" "alb_mysql" {
-  name        = "roboshop-${var.ENV}-mysql-sg"
+resource "aws_security_group" "alb_public" {
+  name        = "roboshop-public-${var.ENV}-mysql-sg"
   description = "Allow 3306 inbound traffic from intranet only"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
