76cd171e3922f481f3a38aecb7f7ed42b7d5e6ff
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:34:18 -0500

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -5,15 +5,9 @@ resource "aws_lb" "alb" {
   load_balancer_type = "application"
   security_groups    =  var.INTERNAL ? : aws_security_group.alb_private.id : aws_security_group.alb_public.id   
 // If this block is called by private-alb module , SG should be private-sg ; If this block is called by public-alb module, SG should be public-sg
-  subnets            = var.INTERNAL ? : data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.P_SUBNET_IDS  
+  subnets            = var.INTERNAL ? : data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS  
 
   enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
 
   tags = {
     Environment = "production"
