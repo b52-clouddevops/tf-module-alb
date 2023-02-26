bd844fdb76505f2cad363dbd85d248f57b5a6978
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:21:47 -0500

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -1,24 +1,24 @@
 # Creates Security Group
 resource "aws_security_group" "alb_public" {
   name        = "roboshop-public-${var.ENV}-alb-sg"
-  description = "Allow 3306 inbound traffic from intranet only"
+  description = "Allows inbound traffic from intranet only"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
     description = "Allow HTTP from Internet"
     from_port   = 80
-    to_port     = var.RDS_MYSQL_PORT
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]   # [] represent's list. 
+    cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
   }
 
-  ingress {
-    description = "Allow RDS From Default VPC Network"
-    from_port   = var.RDS_MYSQL_PORT 
-    to_port     = var.RDS_MYSQL_PORT
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]   # [] represent's list. 
-  }
+#   ingress {
+#     description = "Allow RDS From Default VPC Network"
+#     from_port   = var.RDS_MYSQL_PORT 
+#     to_port     = var.RDS_MYSQL_PORT
+#     protocol    = "tcp"
+#     cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]   # [] represent's list. 
+#   }
 
   egress {
     from_port   = 0
