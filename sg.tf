e1e60fa18a1f3cb9e4f80faa4e4ce2cd4013272f
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:52:55 -0500

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -36,8 +36,8 @@ resource "aws_security_group" "alb_public" {
 
 # Creates Security Group for Private ALB
 resource "aws_security_group" "alb_private" {
-  count       = var.INTERNAL ? 0 : 1
-
+  count       = var.INTERNAL ? 1 : 0
+  
   name        = "roboshop-private-${var.ENV}-alb-sg"
   description = "Allows traffic only from public load-balancer"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
