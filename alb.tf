9def1ec3ba656d253ef852e782732ba176cd4196
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:19:03 -0500

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -1,7 +1,7 @@
 # Creates ALB
 resource "aws_lb" "alb" {
   name               = var.ALB_NAME
-  internal           = 
+  internal           = var.INTERNAL
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [for subnet in aws_subnet.public : subnet.id]
