25596d24116f4fccb43c9093272b554f51651fc5
CodingManoj <mvp2612@hotmail.com>
2023-03-01 19:49:20 -0500

    Auto Push Commit

--- alb-listener.tf
+++ alb-listener.tf
@@ -8,7 +8,11 @@ resource "aws_lb_listener" "private" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
-  }
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "I am operational"
+      status_code  = "200"
+    }
 }
